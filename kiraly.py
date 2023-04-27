a10c79885a0afe455714542219754d3b1b20bf9a
GGGergo <118981096+GGGergo@users.noreply.github.com>
2023-04-27 11:40:24 +0200

    Create kiraly.py

---
+++ kiraly.py
@@ -0,0 +1,4 @@
+class King(Piece):
+    def availableMoves(self,x,y,gameboard, Color = None):
+        if Color is None : Color = self.Color
+        return [(xx,yy) for xx,yy in kingList(x,y) if self.noConflict(gameboard, Color, xx, yy)]
