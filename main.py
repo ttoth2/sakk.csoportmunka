851b7f2000600804959efd4c154fcd3448157e2b
ttoth2 <118882072+ttoth2@users.noreply.github.com>
2023-05-01 21:32:58 +0200

    tábla

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 
-from data.classes.Board import Board
+
 
 pygame.init()
 
--- tábla.py
+++ tábla.py
@@ -1,33 +1,76 @@
 import pygame
-import time
+class Square:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.abs_x = x * width
+        self.abs_y = y * height
+        self.abs_pos = (self.abs_x, self.abs_y)
+        self.pos = (x, y)
+        self.color = 'light' if (x + y) % 2 == 0 else 'dark'
+        self.draw_color = (220, 208, 194) if self.color == 'light' else (53, 53, 53)
+        self.highlight_color = (100, 249, 83) if self.color == 'light' else (0, 228, 10)
+        self.occupying_piece = None
+        self.coord = self.get_coord()
+        self.highlight = False
+        self.rect = pygame.Rect(
+            self.abs_x,
+            self.abs_y,
+            self.width,
+            self.height
+        )
+ def get_coord(self):
+        columns = 'abcdefgh'
+        return columns[self.x] + str(self.y + 1)
 
-FPS=60
-WIDTH, HEIGHT=800,800
-ROWS,COLS=8,8
-SQUARE_SIZE=WIDTH//COLS
+ def draw(self, display):
 
-WHITE=(255,255,255)
-BLACK=(0,0,0)
+        if self.highlight:
+            pygame.draw.rect(display, self.highlight_color, self.rect)
+        else:
+            pygame.draw.rect(display, self.draw_color, self.rect)
 
-WIN=pygame.display.set_mode((WIDTH,HEIGHT))
-def main():
-    run=True
-    ora=pygame.time.Clock()
-    while run:
-        ora.tick(FPS)
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                run=False
+        if self.occupying_piece != None:
+            centering_rect = self.occupying_piece.img.get_rect()
+            centering_rect.center = self.rect.center
+            display.blit(self.occupying_piece.img, centering_rect.topleft)
 
 class Board:
-    def __init__ (self):
-        self.board =[]
-        self.selected.piece=None
-    def draw_squares(self,win):
-        win.fill(BLACK)
-        for row in range(ROWS):
-            for col in range(row % 2,ROWS,2):
-                pygame.draw.rect(win,WHITE,(row*SQUARE_SIZE,col*SQUARE_SIZE,SQUARE_SIZE,SQUARE_SIZE))
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.tile_width = width // 8
+        self.tile_height = height // 8
+        self.selected_piece = None
+        self.turn = 'white'
+        self.config = [
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
+        ]
+        self.squares = self.generate_squares()
+        self.setup_board()
 
+    def generate_squares(self):
+        output = []
+        for y in range(8):
+            for x in range(8):
+                output.append(
+                    Square(x,  y, self.tile_width, self.tile_height)
+                )
+        return output
 
+    def get_square_from_pos(self, pos):
+        for square in self.squares:
+            if (square.x, square.y) == (pos[0], pos[1]):
+                return square
 
+    def get_piece_from_pos(self, pos):
+        return self.get_square_from_pos(pos).occupying_piece
